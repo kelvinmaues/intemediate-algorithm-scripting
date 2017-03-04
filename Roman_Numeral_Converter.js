d399807d2c41f2609078d0954c9565a867e0f26d
[kelvin maues] <[kelvinmaus@gmail.com]>
2017-03-04 15:14:36 -0300

    Roman Numeral Converter

--- Roman_Numeral_Converter.js
+++ Roman_Numeral_Converter.js
@@ -1,10 +1,30 @@
 // Convert the given number into a roman numeral.
+
 function convertToRoman(num) {
- return num;
+  var romanNumbers = {
+  "1": "I",
+  "5": "V",
+  "10": "X",
+  "50": "L",
+  "100": "C",
+  "500": "D",
+  "1000": "M"
+};
+  function splitNumbers(numberToString) {
+    return numberToString.toString().split('');
+  };
+  function inDecimal(arrNumbers) {
+    var length = arrNumbers.length;
+    var decimalArray = [];
+    for (var i = 0; i < length; i++) {
+      console.log(arrNumbers[i] * Math.pow(10, (length-));
+    }
+  }
+  inDecimal(splitNumbers(num));
 }
 
 convertToRoman(36);
-convertToRoman(2); //"II"
+/*convertToRoman(2); //"II"
 convertToRoman(3); //"III"
 convertToRoman(4); //"IV"
 convertToRoman(5); //"V"
@@ -16,3 +36,6 @@ convertToRoman(44); // "XLIV"
 convertToRoman(45); //"XLV"
 convertToRoman(68); //"LXVIII"
 convertToRoman(83); //"LXXXIII"
+convertToRoman(500);
+convertToRoman(649);
+convertToRoman(1000); */
