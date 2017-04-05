c92256bd50fd2540fdbcf31a7ba156e2a2c45a87
Kelvin Maués <kelvinmaus@gmail.com>
2017-04-04 22:06:02 -0300

    new algorithm to solve

--- Missing_letters.js
+++ Missing_letters.js
@@ -1,8 +1,20 @@
 // Coded by Kelvin Maues
 // freeCodeCamp.com
 function fearNotLetter(str) {
-  console.log("c".charCodeAt());
-  return str;
+  // spliting the string in an array
+  var arr = str.split("");
+  // converting each char in UTF-16 code, returning a new array
+  var root = arr.map(function(char){
+  });
+  for (var i = 0; i < root.length; i++) {
+    if (root[i+1] - root[i] == 2){
+      var missedValue = root[i] + 1;
+      console.log("it's missing, " + missedValue)
+    }
+    else{
+      return undefined;
+    }
+  }
 }
 fearNotLetter("abce") //should return "d".
 fearNotLetter("abcdefghjklmno") //should return "i".
