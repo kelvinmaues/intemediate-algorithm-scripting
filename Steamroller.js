a3078d84b0af4b8e8afb0bf640a915467e2e0e0b
“[kelvin <[kelvinmaus@gmail.com]>
2017-05-04 12:14:54 -0300

    Steamroller solved and a new Binary Agents algorithm

--- Steamroller.js
+++ Steamroller.js
@@ -1,7 +1,25 @@
-
+// Coded by Kelvin Maues
+// freeCodeCamp.com
 function steamrollArray(arr) {
   // I'm a steamroller, baby
-  return arr;
+  var array = [];
+  // an recursive function to check is a value in an array is an array or not
+  function isAnArray(value) {
+    if (Array.isArray(value)) {
+      value.filter(isAnArray)
+    }
+    else{
+      array.push(value)
+    }
+  }
+
+  arr.filter(isAnArray);
+  console.log(array)
+  return array;
+
 }
 
-steamrollArray([1, [2], [3, [[4]]]]);
+steamrollArray([[["a"]], [["b"]]]) //should return ["a", "b"].
+steamrollArray([1, [2], [3, [[4]]]]) //should return [1, 2, 3, 4].
+steamrollArray([1, [], [3, [[4]]]]) //should return [1, 3, 4].
+steamrollArray([1, {}, [3, [[4]]]]) //should return [1, {}, 3, 4].
